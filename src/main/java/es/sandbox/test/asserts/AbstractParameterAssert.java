8bd7fcc5e87e605b6a70990b6638403139c74212
Jesús López Alonso <jeslopalo@sandbox.es>
2016-09-22 00:20:52 +0200

    Refactor to AbstractParameterAssert

---
+++ src/main/java/es/sandbox/test/asserts/AbstractParameterAssert.java
@@ -0,0 +1,140 @@
+package es.sandbox.test.asserts;
+
+import org.assertj.core.api.AbstractAssert;
+
+import java.lang.reflect.Executable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Created by jeslopalo on 21/9/16.
+ */
+public class AbstractParameterAssert<A extends AbstractParameterAssert<A, E>, E extends Executable>
+    extends AbstractAssert<A, E> {
+
+    private final ExecutableInvoker<E> executableInvoker;
+
+    public AbstractParameterAssert(E actual, ExecutableInvoker<E> executableInvoker, Class<?> selfType) {
+        super(actual, selfType);
+
+        this.executableInvoker = executableInvoker;
+    }
+
+    /**
+     * @return
+     */
+    public OngoingAssert<A, E> wontThrowAnyException() {
+        return new OngoingAssert(this, this.executableInvoker);
+    }
+
+    /**
+     * @param expected
+     * @return
+     */
+    public OngoingAssert<A, E> willThrow(Class<? extends Throwable> expected) {
+        return new OngoingAssert(this, this.executableInvoker, expected);
+    }
+
+    /**
+     * @return
+     */
+    public OngoingAssert<A, E> willThrowNullPointerException() {
+        return willThrow(NullPointerException.class);
+    }
+
+    /**
+     * @return
+     */
+    public OngoingAssert<A, E> willThrowIllegalArgumentException() {
+        return willThrow(IllegalArgumentException.class);
+    }
+
+    @FunctionalInterface
+    interface ExecutableInvoker<E extends Executable> {
+
+        void invoke(E executable, Object... arguments) throws InstantiationException, IllegalAccessException, InvocationTargetException;
+    }
+
+    public static final class OngoingAssert<T extends AbstractParameterAssert<T, E>, E extends Executable> {
+
+        private final T ongoingAssert;
+        private final Optional<Class<? extends Throwable>> expected;
+        private final ExecutableInvoker<E> executableInvoker;
+
+        OngoingAssert(T ongoingAssert, ExecutableInvoker<E> executableInvoker, Class<? extends Throwable> expected) {
+            this.ongoingAssert = ongoingAssert;
+            this.executableInvoker = executableInvoker;
+            this.expected = Optional.of(expected);
+        }
+
+        OngoingAssert(T ongoingAssert, ExecutableInvoker<E> executableInvoker) {
+            this.ongoingAssert = ongoingAssert;
+            this.executableInvoker = executableInvoker;
+            this.expected = Optional.empty();
+        }
+
+        /**
+         * @param arguments
+         * @return
+         * @throws IllegalAccessException
+         * @throws InstantiationException
+         */
+        public OngoingAssert whenInvokedWith(Object... arguments) throws IllegalAccessException, InstantiationException {
+
+            try {
+                invoke(arguments);
+            } catch (InvocationTargetException exception) {
+                onInvocationException(exception);
+            } catch (Throwable throwable) {
+                onUnexpectedException(throwable);
+            }
+            return this;
+        }
+
+        /**
+         * @return
+         * @throws InstantiationException
+         * @throws IllegalAccessException
+         */
+        public OngoingAssert whenInvokedWithNulls() throws InstantiationException, IllegalAccessException {
+            return whenInvokedWith(new Object[executable().getParameterCount()]);
+        }
+
+        /**
+         * @return
+         * @throws InstantiationException
+         * @throws IllegalAccessException
+         */
+        public OngoingAssert whenInvokedWithoutParams() throws InstantiationException, IllegalAccessException {
+            return whenInvokedWith();
+        }
+
+        private void invoke(Object[] arguments) throws InstantiationException, IllegalAccessException, InvocationTargetException {
+
+            this.executableInvoker.invoke(executable(), arguments);
+
+            if (this.expected.isPresent()) {
+                failBecauseExceptionWasNotThrown(this.expected.get());
+            }
+        }
+
+        private void onInvocationException(InvocationTargetException exception) {
+
+            if (this.expected.isPresent()) {
+                assertThat(exception.getTargetException()).isInstanceOf(this.expected.get());
+            } else {
+                fail("Unexpected exception was thrown", exception.getTargetException());
+            }
+        }
+
+        private void onUnexpectedException(Throwable throwable) {
+            fail(String.format("Unable to find executable %s", executable()), throwable);
+        }
+
+        private E executable() {
+            return (E) this.ongoingAssert.actual;
+        }
+    }
+}
--- src/main/java/es/sandbox/test/asserts/ConstructorAssert.java
+++ src/main/java/es/sandbox/test/asserts/ConstructorAssert.java
@@ -1,126 +1,18 @@
 package es.sandbox.test.asserts;
 
-import org.assertj.core.api.AbstractAssert;
-import org.assertj.core.api.Assertions;
-
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Optional;
 
 /**
  * Created by jeslopalo on 18/9/16.
  */
 public final class ConstructorAssert<T>
-    extends AbstractAssert<ConstructorAssert<T>, Constructor<T>> {
+    extends AbstractParameterAssert<ConstructorAssert<T>, Constructor<T>> {
 
     ConstructorAssert(Constructor<T> actual) {
-        super(actual, ConstructorAssert.class);
+        super(actual, constructorInvoker(), ConstructorAssert.class);
     }
 
-    /**
-     * @return
-     */
-    public OngoingConstructorAssert<T> wontThrowAnyException() {
-        return new OngoingConstructorAssert<>(this);
-    }
-
-    /**
-     * @param expected
-     * @return
-     */
-    public OngoingConstructorAssert<T> willThrow(Class<? extends Throwable> expected) {
-        return new OngoingConstructorAssert<>(this, expected);
-    }
-
-    /**
-     * @return
-     */
-    public OngoingConstructorAssert<T> willThrowNullPointerException() {
-        return willThrow(NullPointerException.class);
-    }
-
-    /**
-     * @return
-     */
-    public OngoingConstructorAssert<T> willThrowIllegalArgumentException() {
-        return willThrow(IllegalArgumentException.class);
-    }
-
-
-    public static final class OngoingConstructorAssert<T> {
-
-        private final ConstructorAssert<T> constructorAssert;
-        private final Optional<Class<? extends Throwable>> expected;
-
-        OngoingConstructorAssert(ConstructorAssert ongoingAssert, Class<? extends Throwable> expected) {
-            this.constructorAssert = ongoingAssert;
-            this.expected = Optional.of(expected);
-        }
-
-        OngoingConstructorAssert(ConstructorAssert ongoingAssert) {
-            this.constructorAssert = ongoingAssert;
-            this.expected = Optional.empty();
-        }
-
-        /**
-         * @param arguments
-         * @return
-         * @throws IllegalAccessException
-         * @throws InstantiationException
-         */
-        public OngoingConstructorAssert whenInvokedWith(Object... arguments) throws IllegalAccessException, InstantiationException {
-
-            try {
-                invoke(arguments);
-            } catch (InvocationTargetException exception) {
-                onInvocationException(exception);
-            } catch (Throwable throwable) {
-                onUnexpectedException(throwable);
-            }
-            return this;
-        }
-
-        /**
-         * @return
-         * @throws InstantiationException
-         * @throws IllegalAccessException
-         */
-        public OngoingConstructorAssert whenInvokedWithNulls() throws InstantiationException, IllegalAccessException {
-            return whenInvokedWith(new Object[constructor().getParameterCount()]);
-        }
-
-        /**
-         * @return
-         * @throws InstantiationException
-         * @throws IllegalAccessException
-         */
-        public OngoingConstructorAssert whenInvokedWithoutParams() throws InstantiationException, IllegalAccessException {
-            return whenInvokedWith();
-        }
-
-        private void invoke(Object[] arguments) throws InstantiationException, IllegalAccessException, InvocationTargetException {
-
-            constructor().newInstance(arguments);
-            if (this.expected.isPresent()) {
-                Assertions.failBecauseExceptionWasNotThrown(this.expected.get());
-            }
-        }
-
-        private void onInvocationException(InvocationTargetException exception) {
-
-            if (this.expected.isPresent()) {
-                Assertions.assertThat(exception.getTargetException()).isInstanceOf(this.expected.get());
-            } else {
-                Assertions.fail("Unexpected exception was thrown", exception.getTargetException());
-            }
-        }
-
-        private void onUnexpectedException(Throwable throwable) {
-            Assertions.fail(String.format("Unable to find constructor %s", constructor()), throwable);
-        }
-
-        private Constructor<T> constructor() {
-            return this.constructorAssert.actual;
-        }
+    private static <E> ExecutableInvoker<Constructor<E>> constructorInvoker() {
+        return (executable, arguments) -> executable.newInstance(arguments);
     }
 }
